4274e40d64d4abdcfd37a6095fd78a426e970379
pliupelis <pliupelis@inbox.lt>
2022-01-03 15:59:52 +0200

    sections

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
 import React from "react";
-import "./App.css";
+
 import {
   Footer,
-  Header,
   Blog,
-  Features,
   Possibility,
+  Features,
   WhatGPT,
+  Header,
 } from "./containers";
 import { CTA, Brand, Navbar } from "./components";
 
-const App = () => {
-  return (
-    <div className="App">
-      <div className="gradient__bg">
-        <Navbar />
-        <Header />
-        <Brand />
-        <WhatGPT />
-        <Features />
-        <Possibility />
-        <CTA />
-        <Blog />
-        <Footer />
-      </div>
+import "./App.css";
+
+const App = () => (
+  <div className="App">
+    <div className="gradient__bg">
+      <Navbar />
+      <Header />
     </div>
-  );
-};
+    <Brand />
+    <WhatGPT />
+    <Features />
+    <Possibility />
+    <CTA />
+    <Blog />
+    <Footer />
+  </div>
+);
 
 export default App;
